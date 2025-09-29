2cb62292fc6847fcca7a7be258f432bf0bac71da
Chris Finn <chris@finnternet.com>
2025-09-28 22:55:46 -0600

    linting and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("prettier"),
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +21,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
+  {
+    plugins: {
+      prettier: (await import("eslint-plugin-prettier")).default,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
